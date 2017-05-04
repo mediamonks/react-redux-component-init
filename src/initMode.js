d9e81f5374232e62912f3a6f8cbbfc15e3224172
Floris Bernard <floris.bernard@gmail.com>
2017-05-04 20:16:44 +0200

    Add JSDoc annotation for initMode #1

--- src/initMode.js
+++ src/initMode.js
@@ -1,2 +1,17 @@
+/**
+ * Mode that is designed for usage on the server. In this mode, this initAction of
+ * each component needs to be executed before it is mounted. This is referred to as
+ * "preparing a component" and can be done using the `prepareComponent()` action.
+ * Mounting a component with an initAction without preparing it first with the same
+ * values for initProps will result in an error.
+ * @type {string}
+ */
 export const MODE_PREPARE = 'MODE_PREPARE';
+/**
+ * Mode that is designed for usage on the client after the initial render has
+ * completed. When a component with an initAction is mounted, it will execute
+ * the initAction. The exact behavior of this depends on the `initSelf` option
+ * passed to `withInitAction()`. See `withInitAction()` for more info.
+ * @type {string}
+ */
 export const MODE_INIT_SELF = 'MODE_INIT_SELF';
