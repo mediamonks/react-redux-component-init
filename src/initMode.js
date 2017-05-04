1b60c3fbe21d7a571d846f6a50ca9cc2b127ee91
Floris Bernard <floris.bernard@gmail.com>
2017-05-04 21:09:49 +0200

    Add JSDoc annotations for selfInitMode #1

--- src/initMode.js
+++ src/initMode.js
@@ -1,4 +1,10 @@
 /**
+ * The possible modes that `react-redux-init` can operate under. One is designed for
+ * the client side, the other for server side rendering. This mode affects
+ * the behavior of the entire module.
+ */
+
+/**
  * Mode that is designed for usage on the server. In this mode, this initAction of
  * each component needs to be executed before it is mounted. This is referred to as
  * "preparing a component" and can be done using the `prepareComponent()` action.
@@ -11,7 +17,8 @@ export const MODE_PREPARE = 'MODE_PREPARE';
  * Mode that is designed for usage on the client after the initial render has
  * completed. When a component with an initAction is mounted, it will execute
  * the initAction. The exact behavior of this depends on the `initSelf` option
- * passed to `withInitAction()`. See `withInitAction()` for more info.
+ * passed to `withInitAction()`. See `withInitAction()` and `initSelfMode`
+ * for more info.
  * @type {string}
  */
 export const MODE_INIT_SELF = 'MODE_INIT_SELF';
--- src/initSelfMode.js
+++ src/initSelfMode.js
@@ -1,4 +1,34 @@
+/**
+ * Possible values for the `selfInit` option of `withInitAction()`
+ */
+
+/**
+ * The component will render immediately, even if `initAction` is still pending.
+ * It is recommended to use this option and render a loading indicator or placeholder content
+ * until `initAction` is resolved. This will give the user immediate feedback that something is
+ * being loaded. While the `initAction` is pending, an `isInitializing` prop will be passed to
+ * the component.
+ * @type {string}
+ */
 export const INIT_SELF_ASYNC = 'ASYNC';
+
+/**
+ * This will cause the component not to be mounted until the first initialization has completed.
+ * The component will remain mounted during any reinitialization.
+ * @type {string}
+ */
 export const INIT_SELF_BLOCKING = 'BLOCKING';
+
+/**
+ * The same behavior as `INIT_SELF_BLOCKING` but it will also unmount the component during
+ * reinitialization.
+ * @type {string}
+ */
 export const INIT_SELF_UNMOUNT = 'UNMOUNT';
+
+/**
+ * Only execute initAction on the server (initMode == MODE_PREPARE). Initialization will
+ * be skipped on the client. This is the opposite of setting `lazy: true` in `withInitAction()`
+ * @type {string}
+ */
 export const INIT_SELF_NEVER = 'NEVER';
