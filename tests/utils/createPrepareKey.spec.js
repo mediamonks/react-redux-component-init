97671ebe34e7ab005ec99a60635b5107bcad06d0
Floris Bernard <floris.bernard@gmail.com>
2017-05-05 23:26:45 +0200

    Improve test naming in createPrepareKey #2

--- tests/utils/createPrepareKey.spec.js
+++ tests/utils/createPrepareKey.spec.js
@@ -1,7 +1,7 @@
 import createPrepareKey from '../../src/utils/createPrepareKey';
 
 describe('createPrepareKey', () => {
-  it('should return equal keys for equal objects with different property orders', () => {
+  it('returns equal keys for equal objects with different property orders', () => {
     const objA = { a: 'a', b: 'b' };
     const objB = { a: 'a', b: 'b' };
     const arrayA = [1, 2, 3];
@@ -10,24 +10,24 @@ describe('createPrepareKey', () => {
     const prepareKeyB = createPrepareKey('Foo', [objB, arrayB]);
     expect(prepareKeyA).toBe(prepareKeyB);
   });
-  it('should return equal keys for equal objects with different property orders', () => {
+  it('returns equal keys for equal objects with different property orders', () => {
     const objA = { a: 'a', b: 'b' };
     const objB = { b: 'b', a: 'a' };
     const prepareKeyA = createPrepareKey('Foo', [objA]);
     const prepareKeyB = createPrepareKey('Foo', [objB]);
     expect(prepareKeyA).toBe(prepareKeyB);
   });
-  it('should return different keys for different componentId', () => {
+  it('returns different keys for different componentId', () => {
     const prepareKeyA = createPrepareKey('Foo', [1, 2, 3]);
     const prepareKeyB = createPrepareKey('Bar', [1, 2, 3]);
     expect(prepareKeyA).not.toBe(prepareKeyB);
   });
-  it('should return different keys for different props', () => {
+  it('returns different keys for different props', () => {
     const prepareKeyA = createPrepareKey('Foo', [1, 2, 3]);
     const prepareKeyB = createPrepareKey('Foo', [1, 0, 3]);
     expect(prepareKeyA).not.toBe(prepareKeyB);
   });
-  it('should return different keys for different prop orders', () => {
+  it('returns different keys for different prop orders', () => {
     const prepareKeyA = createPrepareKey('Foo', [1, 2, 3]);
     const prepareKeyB = createPrepareKey('Foo', [2, 1, 3]);
     expect(prepareKeyA).not.toBe(prepareKeyB);
